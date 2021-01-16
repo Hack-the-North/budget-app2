4f79e27ccbba3ceb16956daa2a0006a5c02a10b4
Steven <wong.steven604@gmail.com>
2021-01-15 23:30:02 -0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void handleText(View v) {
+        TextView t = findViewById(R.id.editTextTextPersonName2);
+        String input = t.getText().toString();
+        Log.d("info", input);
+
+
+    }
+}
